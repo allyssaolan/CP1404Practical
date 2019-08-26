962e1e5820f28e70b272369b108ac1c0239fdc82
allyssa <allyssa.olan@my.jcu.edu.au>
2019-08-26 12:55:59 +1000

    Refractor password program to use functions

--- password.py
+++
@@ -1,6 +0,0 @@
-min_length = 4
-
-password = input("Enter valid password: ")
-while len(password) < min_length:
-    password = input("Invalid password. Please enter a valid password: " )
-print((len(password) * '*'))
